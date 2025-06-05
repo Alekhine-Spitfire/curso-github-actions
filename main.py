e3575cd81da54ca1effcc135d12e4dc7c33481f1
Alejandro Terrazas <enrique01346@gmail.com>
2025-06-05 12:08:16 -0500

    Update main.py me faltó cerrar el paréntesis

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 print("Hola Mundoooooou")
 
-print("Primera impresión para el curso que aprenderé muuy bien de GitHub Actions"
+print("Primera impresión para el curso que aprenderé muuy bien de GitHub Actions")
